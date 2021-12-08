a9ced7aaa884978c092bd80bd47dbd02159e7d8a
Ianis <ianis.chirvasiu@gmail.com>
2021-12-08 20:29:41 +0200

    add homework3 and for lists

---
+++ courses/list.py
@@ -0,0 +1,10 @@
+def print_all_elements(lista):
+    for x in lista:
+      print(x)
+
+
+
+l=[1,2,"a","b"]
+print_all_elements(l)
+
+
---
+++ ianis.py
@@ -0,0 +1,19 @@
+if True and False: # every condition with 'and' & 'false' goes false
+     print ("yes")
+else:
+    print("no")
+
+
+if True or False:
+    print ("True or False=>True")
+else:
+   print ("True or False=>False") # every condition 'or'  & 'True' goes True
+
+
+
+   
+
+
+
+
+
