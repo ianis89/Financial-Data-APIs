7b755e9929a5c42157d3ee10fc49cef62eace5ed
Ianis <ianis.chirvasiu@gmail.com>
2022-04-14 01:36:45 +0300

    Project1

---
+++ Exercitii teme curs live/input 1 exercitii .py
@@ -0,0 +1,104 @@
+
+import sys
+
+print(sys.argv)
+
+#exercitiu 1 /tema
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+
+sum = int (num1) + int (num2)
+
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+#exercitiu1 alta varianta  /tema
+a=int(input("enter first number : "))
+
+b=int(input("enter second number : "))
+
+print("sum of",a, "and",b,"is",a+b)
+
+#exercitiu2 /tema
+num1 = 12
+num2 = 14
+num3 = 20
+
+if (num1 >= num2) and (num1 >= num3):
+    biggest = num1
+elif (num2 >= num1) and (num2 >= num3):
+    biggest = num2
+else:
+    biggest = num3
+print ("The biggest number is " , biggest)
+
+#exercitiu 3
+
+name =  input ("Enter your name :")
+age = int (input ("Enter your age :"))
+print(name)
+print ("Nice to meet you")
+if age >= 18:
+    print ("you are allowed to drink")
+
+
+#exercitiu4
+sentence = input("Type a sentence of at least 5 words")
+words = sentence.split()
+if len(words) < 6:
+    print("You need to type more words!")
+
+
+#exercitiu 5
+
+import random
+number = random.randint(0,9)
+
+print("Try to guess  the number (between 0  and 9) in three guesses! ")
+
+guess1 = int(input(" Enter you first guess:"))
+
+if guess1 ==  number:
+    print ("you guessed it on the first try!!! You must be good !!")
+    sys.exit()
+elif guess1 > number:
+    print ("your guess is to high!")
+else:
+    print ("your guess is to low !")
+
+
+guess2 = int (input(" Enter you second  guess:"))
+if guess2 ==number:
+    print ("you guessed on the second try! Nice job ")
+    sys.exit()
+elif guess2 > number:
+    print ("your guess is to high!")
+else:
+    print ("your guess is to low !")
+
+guess3 = int (input(" Enter you third  guess:"))
+if guess3 ==number:
+    print ("you guessed it! Great Job!!")
+    sys.exit()
+elif guess3 > number:
+    print ("your guess is to high!")
+else:
+    print ("your guess is to low !")
+
+
+
+
+
+#############################################################################
+
+
+######################################
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+
+if n1 > n2:
+    diff = n1 - n2
+else:
+    diff = n2 - n1
+
+print("The difference is:",diff)
---
+++ courses/Proba Examen/proba practica examen 1.py
@@ -0,0 +1,37 @@
+#
+# Modelaţi cu ajutorul limbajului Python următoarea problemă:
+#
+# O  persoană este interesată de  achiziţionarea  unui  automobil. Ştim  că orice  pasionat  de automobile îşi îndreaptă atenţia asupra motorului, fiind interesat de:
+#
+# -număr cilindri (int)
+# -număr supape (int)
+# -putere maximă - cp (int)
+# -tip carburant (string)
+# -tip injecţie (string)
+# -catalizator (string)
+# -sistem de alimentare (string)
+#
+# Cumpărătorul  mai  este  interesat  de  culoarea  maşinii (string), firma producătoare (string) şi de alte dotări:
+#
+# -vopsea metalizată (boolean)
+# -aer condiţionat (boolean)
+# -sistem de frânare (string)
+# -airbag şofer (boolean)
+# -faruri dublu optice (boolean)
+#
+#
+# Implementaţi clasele Motor şi Automobil ştiind că aceasta din urmă moşteneşte clasa Vehicul
+# care are următoarele atribute: număr roţi (int), număr uşi (int), număr locuri (int);
+#  capacitate rezervor (int) /portbagaj (int) u.m. : litru; dimensiuni: lungime (int) / lăţime(int); înălţime(int)  u.m.:  mm;
+#  consum: urban/extraurban/mixt (int) u.m.:litri/100km.
+#
+# Să se implementeze   constructorii   corespunzători   claselor   definite   anterior,
+# să se declare metodele  get şi set  pentru  variabilele  private  ale  claselor,
+# să se  realizeze  o  metodă de afişare a caracteristicilor fiecărui automobil.
+#
+# Într-o nouă clasă numită Achiziţie declaraţi 7 obiecte de tip Automobil.
+#
+# Realizaţi o metodă care pe baza unor date de intrare citite de la   tastatură să afişeze automobilul / automobilele
+# care corespund  cererii utilizatorului (potenţialului cumpărător).
+#
+
