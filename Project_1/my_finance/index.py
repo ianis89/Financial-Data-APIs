41a45482fca3da7c2ec89a7590a4e78e214f08cc
Ianis <ianis.chirvasiu@gmail.com>
2022-05-11 17:04:19 +0300

    Project1

--- Project_1/my_finance/index.py
+++ Project_1/my_finance/index.py
@@ -24,10 +24,10 @@ from my_finance.api.health import health_router
 from my_finance.api.diagrams import diagrams_router
 
 app = FastAPI(
-    title="Name of our app",  # TODO for homework, name your application
+    title="Financial-Data-APIs",  # TODO for homework, name your application
     # <major_version>.<minor_version>.<patch_version>
-    version="0.2.0",  # increase version after finishing homework
-    description="",  # TODO add a description
+    version="1.2.0",  # increase version after finishing homework
+    description="A collection of python modules to get stock data from Yahoo Finance",  # TODO add a description
 )
 app.include_router(stocks_router)
 app.include_router(health_router)
